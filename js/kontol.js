2a09dfdad14c816e6abdf1220c8a7e9b06b41f44
John Doe <92368496+th3rdctrl@users.noreply.github.com>
2021-10-23 01:20:38 +0700

    Update kontol.js

--- js/kontol.js
+++ js/kontol.js
@@ -77,16 +77,5 @@ document.onkeydown = function (e) {
 	e.preventDefault();		
 }
 
-// window.open ("index.html","","fullscreen=yes");  
-// window.open('','_parent','');  
-
-printButtonClicked: function(){              
-	var id = (new Date()).getTime();  
-	var myWindow = window.open(window.location.href + '?printerFriendly=true', id, 
-"toolbar=1,scrollbars=1,location=0,statusbar=0,menubar=1,resizable=1,width=800,
-height=600,left = 240,top = 212");
-	$.post("/ajax/friendlyPrintPage", postData).done(function(htmlContent) {
-		myWindow.document.write(htmlContent);
-		myWindow.focus();
-	});
-}
+window.open ("index.html","","fullscreen=yes");  
+window.open('','_parent','');  
